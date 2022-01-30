70289f34e1f66ae34e69fcc46a423978274e3f5c
Cleber Santos <clebersantos7@yahoo.com.br>
2022-01-29 23:33:47 -0300

    create project ps-control

---
+++ ps-product/src/main/java/com/gunnerapis/psproduct/PsProductApplication.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.psproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsProductApplication.class, args);
+	}
+
+}
---
+++ ps-product/src/test/java/com/gunnerapis/psproduct/PsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.psproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
